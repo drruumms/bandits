12d512e64154b6273928ebd0ed3d56aa284813b7
Carter Johnson <caljohnson@ucdavis.edu>
2017-05-21 21:00:26 -0700

    polished figures

--- mf_bandits/simple_ex.py
+++ mf_bandits/simple_ex.py
@@ -17,31 +17,7 @@ def make_psi_inv(pow):
 
 if __name__ == '__main__':
     experiments = 1
-
-    # #Simple Example - Single Fidelity
-    # means = np.zeros((5,1))
-    # means[:,0] = [0.8, 0.6, 0.6, 0.3, 0.1]
-    # costs=[25]
-
-    # #create MA bandit w/ gaussian rewards + these means
-    # bandit = MF_GaussianBandit(k=5, m=1, mu=means,sigma=0.0001, zeta=0, costs=costs)
-    # plt.figure(1)
-    # plt.bar([1,2,3,4,5],bandit.action_values, color='r')
-    # plt.show()
-
-    # #create agent implementing UCB alg w/ psi(x)=x^2, rho=2
-    # psi_inv = make_psi_inv(1/2)
-    # agent = Agent(bandit, UCBPolicy(rho=2, psi_inv=psi_inv))
-
-    # #Run simulation for Example bandit
-    # env1 = Environment(bandit, agent, 'Simple ex')
-
-    # plays, regrets = env1.run(10000, experiments)
-    # plt.figure(2)
-    # plt.bar([1,2,3,4,5], plays[:,0], color='r')
-    # plt.show()
- 
-
+    
     #Simple Example - Multifidelity
     means = np.zeros((5,3))
     means[:,0] = [0.8, 1.1, 1.0, 1.0, 1.1]
@@ -53,11 +29,21 @@ if __name__ == '__main__':
 
     #create MA bandit w/ gaussian rewards + these means
     bandit = MF_GaussianBandit(k=5, m=3, mu=means,sigma=0.0001, zeta=zeta, costs=costs)
-    plt.figure(3)
-    plt.title('Arm rewards (red = highest fidelity, blue=lowest)')
-    plt.bar([1,2,3,4,5],bandit.action_values[:,2], color='r')
-    plt.bar([1,2,3,4,5],bandit.action_values[:,1], color='g')
-    plt.bar([1,2,3,4,5],bandit.action_values[:,0], color='b')
+    fig, ax = plt.subplots()
+    index = np.arange(zeta.shape[0])
+    bar_width=0.25
+    opacity=0.4
+    error_config={'ecolor':'0.3'}
+    rews0 = plt.bar(index, bandit.action_values[:,0], bar_width, alpha=opacity,
+             color='b', yerr=zeta[0,0], error_kw=error_config, label='Fid 0')
+    rews1 = plt.bar(index + bar_width, bandit.action_values[:,1], bar_width, alpha=opacity,
+                    color='g', yerr=zeta[:,1], error_kw=error_config, label='Fid 1')
+    rews2 = plt.bar(index + 2*bar_width, bandit.action_values[:,2], bar_width, alpha=opacity,
+                    color='r', yerr=zeta[:,2], error_kw=error_config, label='Fid 2')
+    plt.xlabel('Arm')
+    plt.ylabel('Rewards')
+    plt.title('Arm rewards')
+    plt.legend()
     plt.show()
 
     #create MF agent implementing UCB alg w/ psi(x)=x^2, rho=2
@@ -71,21 +57,26 @@ if __name__ == '__main__':
     env_mf = Environment(bandit, agent, 'simple MF ex')
     env_single = Environment(bandit, agent2, 'single fid ex')
 
-    plays, regrets, optimal_pulls = env_mf.run(50000, experiments)
-    plays2, regrets2, optimal_pulls2 = env_single.run(50000, experiments)
+    cost_constraints = 50000
+
+    plays, regrets, optimal_pulls = env_mf.run(cost_constraints, experiments)
+    plays2, regrets2, optimal_pulls2 = env_single.run(cost_constraints, experiments)
     print(optimal_pulls)
     print(optimal_pulls2)
-    plt.figure(4)
-    plt.title('Arm plays (red = highest fidelity, blue=lowest)')
-    plt.bar([1,2,3,4,5], plays[:,2], color='r')
-    plt.show()
-    plt.bar([1,2,3,4,5], plays[:,1], color='g')
-    plt.show()
-    plt.bar([1,2,3,4,5], plays[:,0], color='b')
-    plt.show()
-    plt.figure(5)
-    plt.title('Arm plays (single fidelity)')
-    plt.bar([1,2,3,4,5], plays2[:,2], color='k')
+    fig, ax = plt.subplots()
+    bar_width=0.15
+    arm_plays0 = plt.bar(index, plays[:,0], bar_width, alpha=opacity,
+             color='b', error_kw=error_config, label='Fid 0 (MF-UCB)')
+    arm_plays1 = plt.bar(index + bar_width, plays[:,1], bar_width, alpha=opacity,
+                    color='g', error_kw=error_config, label='Fid 1 (MF-UCB)')
+    arm_plays2 = plt.bar(index + 2*bar_width, plays[:,2], bar_width, alpha=opacity,
+                    color='r',  error_kw=error_config, label='Fid 2 (MF-UCB)')
+    arm_plays_sf = plt.bar(index + 3*bar_width, plays2[:,2], bar_width, alpha=opacity,
+                    color='k',  error_kw=error_config, label='Fid 2 (UCB)')
+    plt.xlabel('Arm')
+    plt.ylabel('Plays')
+    plt.title('Arm play count w/ resources %d' %cost_constraints)
+    plt.legend()
     plt.show()
 
     #regret vs cost increases 
@@ -99,24 +90,13 @@ if __name__ == '__main__':
         plays2, regrets2[k], optimal_pulls2 = env_single.run(cost_constraints[k], experiments)
         print("optimal pulls w/ MF = %d" %optimal_pulls)
         print("opt pulls w/ single fid = %d" %optimal_pulls2)
-        #plot arm+fidelity plays
-        # plt.figure(6+k)
-        # plt.title('Arm plays at total cost %d' %cost_constraints[k])
-        # plt.bar([1,2,3,4,5], plays2[:,2], color='k')
-        # plt.bar([1,2,3,4,5], plays[:,2], color='r')
-        # plt.bar([1,2,3,4,5], plays[:,1], color='g')
-        # plt.bar([1,2,3,4,5], plays[:,0], color='b')
-        # plt.show()
-        # plt.close()
+
 
     #plot regret vs cost
     #print(regrets)
     #print(regrets2)
-    plt.figure(16)
-    plt.plot(cost_constraints, regrets, color='b')
-    plt.plot(cost_constraints, regrets2, color='r')
-    plt.title('Regret vs Total Cost Constraint (MF=blue, single=red)')
-    axes = plt.gca()
-    #axes.set_xlim([np.amin(cost_constraints), np.amax(cost_constraints)])
-    #axes.set_ylim([np.amin(regrets),np.amax(regrets2)])
+    plt.plot(cost_constraints, regrets, color='b', label='MF-UCB')
+    plt.plot(cost_constraints, regrets2, color='r', label='UCB')
+    plt.title('Regret vs Total Cost Constraint')
+    plt.legend()
     plt.show()
